b8353d8f1c92ec70fe33c4a340fe89657f7d6570
Aleksei Naumyshev <naumyshev@yandex.ru>
2022-06-07 21:01:01 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <Todolist/>
+        </div>
+    );
+}
+
+const Todolist = () => {
+    return (
+        <div>TODOLIST</div>
+    )
+}
+
+export default App;
