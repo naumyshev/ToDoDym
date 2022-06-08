0403008d7962e559274ad1b63ba5702cf6117a68
Aleksei Naumyshev <naumyshev@yandex.ru>
2022-06-08 07:11:35 +0300

    added Todolist.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Todolist} from "./Todolist";
 
 function App() {
+
     return (
         <div className="App">
             <Todolist/>
+            <Todolist/>
+            <Todolist/>
         </div>
     );
-}
-
-const Todolist = () => {
-    return (
-        <div>TODOLIST</div>
-    )
 }
 
 export default App;
