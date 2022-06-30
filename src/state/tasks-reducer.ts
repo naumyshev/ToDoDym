49c52311aa50e6b587e95fb91ff0943f515c3389
Aleksei Naumyshev <naumyshev@yandex.ru>
2022-06-30 19:33:18 +0300

    #09 added test for remove task

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -15,8 +15,21 @@ type ActionsType = Action1Type | Action2Type
 export const tasksReducer = (state: TasksStateType, action: ActionsType): TasksStateType => {
 
     switch (action.type) {
-
+        case "1": {
+            return {...state}
+        }
+        case "2": {
+            return {...state}
+        }
         default:
             return state
     }
+}
+
+export const action1AC = (): Action1Type => {
+    return { type: "1", id: ''}
+}
+
+export const action2AC = (): Action2Type => {
+    return { type: "2", title: ''}
 }
