81b5cf0b153c2653946839661b3e6a530f26642f
Aleksei Naumyshev <naumyshev@yandex.ru>
2022-08-19 21:51:34 +0300

    refactored tasks-reducer.test.ts

--- src/state/tasks-reducer.test.ts
+++ src/state/tasks-reducer.test.ts
@@ -1,5 +1,4 @@
 import {TasksStateType} from "../App";
-import {v1} from "uuid";
 import {removeTaskAC, tasksReducer} from "./tasks-reducer";
 
 test('correct task should be deleted from correct array', () => {
@@ -16,14 +15,10 @@ test('correct task should be deleted from correct array', () => {
         ]
     }
 
-
     const action = removeTaskAC('2', 'todolistId2')
-
     const endState = tasksReducer(startState, action)
 
     expect(endState['todolistId1'].length).toBe(3)
     expect(endState['todolistId2'].length).toBe(2)
     expect(endState['todolistId2'].every(t => t.id !== '2')).toBe(true)
-
-
 })
